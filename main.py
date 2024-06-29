8a8a657ec80d7f172dcbe9161073d083bc7e864b
Ian Cass <ian@wheep.co.uk>
2024-06-29 21:15:42 +0100

    updates

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 #
 # Always run behind Nginx or Apache
 #
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import psycopg2
@@ -16,13 +15,30 @@ app = FastAPI()
 
 class Star(BaseModel):
     source_id: int
-    random_index: int
     ra: float
     dec: float
     pmra: Optional[float]
     pmdec: Optional[float]
     phot_g_mean_mag: float
     phot_bp_mean_mag: float
+
+class Spectra(BaseModel):
+    source_id: int
+    wavelength: List[float]
+    flux: List[float]
+    flux_error: List[float]
+
+class StarsAndSpectra(BaseModel):
+    source_id: int
+    ra: float
+    dec: float
+    pmra: Optional[float]
+    pmdec: Optional[float]
+    phot_g_mean_mag: float
+    phot_bp_mean_mag: float
+    wavelength: List[float]
+    flux: List[float]
+    flux_error: List[float]
 
 def get_db_connection():
     conn = psycopg2.connect(
@@ -33,27 +49,58 @@ def get_db_connection():
     )
     return conn
 
-@app.get("/star", response_model=Star)
+@app.get("/star/{source_id}", response_model=Star)
 def get_star_by_id(source_id: int):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("SELECT * FROM stars WHERE source_id = %s", (source_id,))
+    cursor.execute("SELECT source_id, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag FROM stars WHERE source_id = %s", (source_id,))
     star = cursor.fetchone()
     cursor.close()
     conn.close()
     if star is None:
-        raise HTTPException(status_code=404, detail="Star not found")
+        raise HTTPException(status_code=404, detail="Star data not found")
     return star
 
-@app.get("/search", response_model=List[Star])
-def cone_search(ra: float, dec: float, radius: float, mag: float):
+@app.get("/spectra/{source_id}", response_model=Spectra)
+def get_spectra_by_id(source_id: int):
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM spectra WHERE source_id = %s", (source_id,))
+    spectra = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if spectra is None:
+        raise HTTPException(status_code=404, detail="Spectra data not found")
+
+    return spectra;
+
+@app.get("/stars", response_model=List[Star])
+def star_search(ra: float, dec: float, radius: float, mag: float):
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
     query = """
-    SELECT source_id, random_index, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag
+    SELECT source_id, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag
     FROM stars
-    WHERE sphere_point @ scircle(spoint(radians(%s), radians(%s)), radians(%s))
+    WHERE sphere_point <@ scircle(spoint(radians(%s), radians(%s)), radians(%s))
     AND phot_g_mean_mag <= %s
+    LIMIT 5000
+    """
+    cursor.execute(query, (ra, dec, radius, mag))
+    stars = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return stars
+
+@app.get("/spectras", response_model=List[StarsAndSpectra])
+def spectra_search(ra: float, dec: float, radius: float, mag: float):
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    query = """
+    SELECT st.source_id, st.ra, st.dec, st.pmra, st.pmdec, st.phot_g_mean_mag, st.phot_bp_mean_mag, sp.wavelength, sp.flux, sp.flux_error
+    FROM stars st, spectra sp
+    WHERE st.sphere_point <@ scircle(spoint(radians(%s), radians(%s)), radians(%s))
+    AND st.phot_g_mean_mag <= %s
+    AND st.source_id = sp.source_id
     LIMIT 5000
     """
     cursor.execute(query, (ra, dec, radius, mag))
