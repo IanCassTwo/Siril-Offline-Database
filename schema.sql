935c3d1233c5ff483b05d7dda472bdf815a7f8f6
IanCassTwo <30568591+IanCassTwo@users.noreply.github.com>
2024-07-01 17:34:38 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE stars (
     sphere_point spoint
     is_xp_sampled BOOLEAN
 );
+COPY stars (random_index, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag, source_id) FROM '/path/to/your/file.csv' DELIMITER ',' CSV HEADER;
 UPDATE stars SET sphere_point = spoint(radians(ra), radians(dec));
 CREATE INDEX stars_sidx ON stars USING gist (sphere_point);
 CREATE INDEX idx_phot_g_mean_mag ON stars (phot_g_mean_mag)
