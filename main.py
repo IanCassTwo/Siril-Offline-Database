67e3d9ad2eed19cd5e8caa7975e0db1828d13306
Ian Cass <ian@wheep.co.uk>
2024-07-05 09:55:09 +0100

    Change to a single consolidated table for stars and spectra

--- main.py
+++ main.py
@@ -17,25 +17,15 @@ class Star(BaseModel):
     pmra: Optional[float]
     pmdec: Optional[float]
     phot_g_mean_mag: float
-    phot_bp_mean_mag: float
 
 class Spectra(BaseModel):
-    source_id: int
-    wavelength: List[float]
-    flux: List[float]
-    flux_error: List[float]
-
-class StarAndSpectra(BaseModel):
     source_id: int
     ra: float
     dec: float
     pmra: Optional[float]
     pmdec: Optional[float]
     phot_g_mean_mag: float
-    phot_bp_mean_mag: float
-    wavelength: List[float]
     flux: List[float]
-    flux_error: List[float]
 
 async def get_db_connection():
     return await asyncpg.connect('postgresql://stars:stars@localhost:5432/stars')
@@ -73,20 +63,20 @@ async def fetch_one(conn, query: str, model_class: Type[ModelType], *args):
 @app.get("/spectra/{source_id}", response_model=Spectra)
 async def get_spectra_by_id(source_id: int):
     conn = await get_db_connection()
-    query = "SELECT * FROM spectra WHERE source_id = $1"
+    query = "SELECT * FROM stars WHERE source_id = $1"
     return await fetch_one(conn, query, Spectra, source_id)
 
 @app.get("/star/{source_id}", response_model=Star)
 async def get_star_by_id(source_id: int):
     conn = await get_db_connection()
-    query = "SELECT source_id, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag FROM stars WHERE source_id = $1"
+    query = "SELECT * FROM stars WHERE source_id = $1"
     return await fetch_one(conn, query, Star, source_id)
 
 @app.get("/stars", response_model=List[Star])
 async def star_search(ra: float, dec: float, radius: float, mag: float):
     conn = await get_db_connection()
     query = """
-    SELECT source_id, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag
+    SELECT *
     FROM stars
     WHERE sphere_point <@ scircle(spoint(radians($1), radians($2)), radians($3))
     AND phot_g_mean_mag <= $4
@@ -94,15 +84,14 @@ async def star_search(ra: float, dec: float, radius: float, mag: float):
     """
     return StreamingResponse(fetch_many(conn, query, Star, ra, dec, radius, mag), media_type="application/json")
 
-@app.get("/spectras", response_model=List[StarAndSpectra])
+@app.get("/spectras", response_model=List[Spectra])
 async def spectra_search(ra: float, dec: float, radius: float, mag: float):
     conn = await get_db_connection()
     query = """
-    SELECT st.source_id, st.ra, st.dec, st.pmra, st.pmdec, st.phot_g_mean_mag, st.phot_bp_mean_mag, sp.wavelength, sp.flux, sp.flux_error
-    FROM stars st, spectra sp
-    WHERE st.sphere_point <@ scircle(spoint(radians($1), radians($2)), radians($3))
-    AND st.phot_g_mean_mag <= $4
-    AND st.source_id = sp.source_id
+    SELECT *
+    FROM stars
+    WHERE sphere_point <@ scircle(spoint(radians($1), radians($2)), radians($3))
+    AND phot_g_mean_mag <= $4
     LIMIT 5000
     """
-    return StreamingResponse(fetch_many(conn, query, StarAndSpectra, ra, dec, radius, mag), media_type="application/json")
+    return StreamingResponse(fetch_many(conn, query, Spectra, ra, dec, radius, mag), media_type="application/json")
